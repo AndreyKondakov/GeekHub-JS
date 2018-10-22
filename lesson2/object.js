502b795a195d78b51a8c7566f80829cf010f43b4
AndreyKondakov <primax7k@gmail.com>
2018-10-23 01:06:09 +0300

    method object: push, pop, find

--- lesson2/object.js
+++ lesson2/object.js
@@ -1,9 +1,42 @@
 var cat = {
-  color: "red",
-  sex: 'male',
-  name: 'Redbull'
+  0: 'color: "red"',
+  1: "sex: 'male'",
+  2: "name: 'Redbull'"
+};
+var count = 3;
+console.log(cat);
+
+// push
+console.log('push:');
+cat[3] = 'maxSpeed: "14 km/h"';
+console.log(cat);
+
+// #2
+cat.push = function (value) {
+  return cat[count++] = value
 };
 
+cat.push('push some info');
 console.log(cat);
+cat.push('push more info');
+
+console.log(cat);
+console.log(cat.push.toString());
+
+// pop
+console.log('pop:');
+cat.pop = function () {
+  return delete cat[--count]
+};
+cat.pop();
+console.log(cat);
+
+// find:
+console.log('find:');
+cat.find = function (value) {
+  return cat[value]
+};
+console.log(cat.find(0));
+
 
 
