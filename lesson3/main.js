b5bf1fc8284e4a6a5210e171ecfa85f504d2b1f9
AndreyKondakov <primax7k@gmail.com>
2018-10-31 10:01:49 +0200

    lesson 3: 3task #1

--- lesson3/main.js
+++ lesson3/main.js
@@ -8,3 +8,44 @@ Number.prototype.num = function (b) {
 5..num(5);
 10..num(7);
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Training
+console.log('Training:');
+// sum all numbers: example 4 = 4 + 3 + 2 + 1 (10)
+// #1 using cycle
+function sumTo(n) {
+  var result = n;
+  for (var i = 1; i < n; i++) {
+    result += n - i
+  }
+  return result
+}
+console.log(sumTo(4));
+
+// #2 using recursion
+function sumToRecursion(n) {
+  return n + sumTo(n-1)
+}
+console.log(sumToRecursion(100));
+
+// #3 using arithmetic progression.
+
+function mathProgress(n) {
+  return n*(n+1)/2;
+}
+console.log(mathProgress(4));
+
+
---
+++ lesson3/task3/task/task1.js
@@ -0,0 +1,61 @@
+'use strict';
+
+/**
+ * Числа Фиббоначи
+ * 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765
+ * https://en.wikipedia.org/wiki/Fibonacci_number
+ *
+ * Знайти суму перших n чисел фібоначі.
+ */
+
+var fibonacciTests = [
+    {
+        parameters: [1],
+        expectedResult: 1
+    },
+    {
+        parameters: [3],
+        expectedResult: 4
+    },
+    {
+        parameters: [5],
+        expectedResult: 12
+    },
+    {
+        parameters: [20],
+        expectedResult: 17710
+    },
+    {
+        parameters: [0],
+        expectedResult: 0
+    }
+];
+
+
+function fibonacci(n) {
+    if (n == 1) {
+        return n
+    } else if (n == 0) {
+        return n
+    } else {
+      var a = 1,
+        b = 1;
+      var save = 2;
+      for (var i = 3; i <= n; i++) {
+        var c = a + b;
+        a = b;
+        b = c;
+        save += c;
+      }
+      return save;
+    }
+}
+
+
+tasks.push({
+    title: "Числа Фиббоначи",
+    solution: fibonacci,
+    tests: fibonacciTests
+});
+
+console.log(fibonacci(20));
