5da890408e1de33364953e64f02a5802fbc9b793
AndreyKondakov <primax7k@gmail.com>
2018-11-08 10:11:27 +0200

    lesson 4: help)

---
+++ lesson4/main.js
@@ -0,0 +1,56 @@
+// let result = arr.reduce(function (a, b) {
+//
+//   var sum = 0
+//   var difference = 0
+//   var c = 0
+//
+//   if (a > b) {
+//     difference = a - b;
+//     sum = difference
+//
+//   } else if (a < b && difference != 0) {
+//     c = b - a;
+//     // return 0
+//
+//     if (c > difference) {
+//       return c - difference
+//     }
+//     else {
+//       // return 0
+//     }
+//
+//
+//   } else {
+//     return b
+//   }
+//
+//   sum += difference
+//   return sum
+//
+//
+//
+// });
+
+console.log(result);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
