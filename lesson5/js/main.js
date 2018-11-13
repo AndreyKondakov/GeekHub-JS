36b8420e894328d0e9a8987009f055738fb7373a
AndreyKondakov <primax7k@gmail.com>
2018-11-13 22:33:02 +0200

    lesson 5: reverse items

--- lesson5/js/main.js
+++ lesson5/js/main.js
@@ -1,71 +1,54 @@
 (function () {
-'use strict';
+  'use strict';
 
-let todo = document.getElementById('todo');
-let buttonAdd = todo.querySelector('button');
-let list = todo.querySelector('ul');
-let inputTask = todo.querySelector('input[type=text]');
+  let todo = document.getElementById('todo');
+  let buttonAdd = todo.querySelector('button');
+  let list = todo.querySelector('ul');
+  let inputTask = todo.querySelector('input[type=text]');
 
-
-buttonAdd.addEventListener('click', function () {
-  let task = inputTask.value;
-  if (task !== '') {
-    createItem(list, task);
-    inputTask.value = '';
-  }
-});
-
-function createItem(list, text) {
-  const item = document.createElement('li');
-  item.className = 'list-group-item';
-
-  const span = document.createElement('span');
-  span.textContent = text;
-  item.appendChild(span);
-  list.appendChild(item);
-
-  const btn = document.createElement('button');
-  btn.className = 'btn btn-del';
-  btn.textContent = 'x'
-  item.appendChild(btn)
-}
-
-// let buttonDel = list.querySelector('button');
-//   let buttonDel = todo.getElementsByClassName('btn-del');
-
-// buttonDel.addEventListener('click', function () {
-  // let item = todo.querySelector('li')
-//   // let thisItem = list.
-//   console.log('text')
-
-//   // list.removeChild(item)
-//
-
-//   buttonDel.remove.previousSibling
-
-// });
-
-  // remove.addEventListener('click', function () {
-  //   removeTask(this);
-  // });
-
-//
-// let item = document.querySelector('li');
-// let buttonDel = document.querySelector('button');
-//
-//   item.onclick = function () {
-//   list.removeChild(this)
-// }
-//
-
-list.addEventListener('click', ({ target }) => {
-    if (target.tagName === 'BUTTON') {
-      list.removeChild(target.parentNode);
+  buttonAdd.addEventListener('click', function () {
+    let task = inputTask.value;
+    if (task !== '') {
+      createItem(list, task);
+      inputTask.value = '';
     }
   });
 
+  function createItem(list, text) {
+    const item = document.createElement('li');
+    item.className = 'list-group-item';
 
+    const span = document.createElement('span');
+    span.textContent = text;
+    item.appendChild(span);
+    list.appendChild(item);
 
+    const div = document.createElement('div');
+    item.appendChild(div);
 
+    const spanData = document.createElement('span');
+    const data = (new Date()).toString().split(' ').splice(1, 4).join(' ');
+    spanData.textContent = data;
+    div.appendChild(spanData)
+
+    const btn = document.createElement('button');
+    btn.className = 'btn btn-del';
+    btn.textContent = 'x'
+    div.appendChild(btn)
+  }
+  // del item on click
+  list.addEventListener('click', ({target}) => {
+    if (target.tagName === 'BUTTON') {
+      list.removeChild(target.parentNode.parentNode);
+    }
+  });
+
+  // reverse todo list
+  document.getElementById('reverse').onclick = function () {
+    let items = list.querySelectorAll('li');
+    for (let i = items.length -2; i >= 0; i--) {
+      list.appendChild(items[i])
+    }
+  }
 
 }());
