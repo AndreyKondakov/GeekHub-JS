def66b8e725c2ffa1365fe7fd9b50d8630a1c1f6
AndreyKondakov <primax7k@gmail.com>
2018-11-27 20:23:10 +0200

    lesson 6 v2: update timer: visual item

--- lesson6v2/js/main.js
+++ lesson6v2/js/main.js
@@ -12,20 +12,25 @@ class Character {
     // this.createItem(name);
     // this.counter();
 
-    // делаем counter в конструкторе , что бы время было свое у каждого нового обьекта
-
+    // делаю counter в конструкторе , что бы время было свое у каждого нового обьекта
     this.counter = setInterval(() => {
+      // document.getElementsByClassName('character-name').style.color = "blue";  //for test
       this.walk  = this.walk - 5;
       console.log('test walk', this.walk);
-      
+
+      let life = document.getElementsByClassName('progress-life')[0].style.width = `${this.walk} + %`;
+      let testik = document.getElementById('add-name').style.color = 'red';
       this.eat =   this.eat - 5;
       console.log( this.eat);
+      let saturation = document.getElementsByClassName('progress-saturation')[0].style.width = `${this.eat} + %`;
 
       this.sleep = this.sleep - 5;
       console.log( this.sleep);
+      let strength = document.getElementsByClassName('progress-strength')[0].style.width = `${this.sleep} + %`;
 
       this.play =  this.play - 5;
       console.log( this.play);
+      let happiness = document.getElementsByClassName('progress-happiness')[0].style.width = `${this.play} + %`;
 
       if ((this.walk && this.eat && this.sleep && this.play) <= 0 ) {
         clearTimeout(this.counter);
@@ -33,27 +38,6 @@ class Character {
 
     }, 2000);
   }
-  // counter () {
-  //   setInterval(() => {
-  //     this.walk  = this.walk - 5;
-  //     console.log(this.walk);
-  //     this.eat =   this.eat - 5;
-  //
-  //     this.sleep = this.sleep - 5;
-  //
-  //     this.play =  this.play - 5;
-  //   }, 2000);
-  // }
-
-  // createItem(name) {
-  //   const character = document.getElementById('character');
-  //   const items = character.children;
-  //   const mainItem = items[0];
-  //   const itemCopy = mainItem.cloneNode(true);
-  //   itemCopy.querySelector('span').innerHTML = `${name}`;
-  //
-  //   character.appendChild(itemCopy);
-  // }
 
   initCharacter () {
     const addName = document.getElementById('add-name');
@@ -82,22 +66,6 @@ class Character {
     })
   }
 
-  // initCharacter () {
-  //   const addName = document.getElementById('add-name');
-  //   let inputName = addName.querySelector('input[type=text]');
-  //   let buttonAdd = addName.querySelector('button');
-  //
-  //   buttonAdd.addEventListener('click', function () {
-  //     let newName = inputName.value;
-  //     if (newName !== '') {
-  //       // this.createItem(newName);
-  //
-  //       inputName.value = '';
-  //     }
-  //   })
-  // }
-
-
   // walk() {
   //
   // }
@@ -113,12 +81,47 @@ class Character {
 }
 //
 // let ted = 'ted';
-//
 // var test = new Character(`${ted}`);
 // console.log(test.name);
 
 new Character();
 
-
-
 })();
+
+
+// counter () {
+//   setInterval(() => {
+//     this.walk  = this.walk - 5;
+//     console.log(this.walk);
+//     this.eat =   this.eat - 5;
+//
+//     this.sleep = this.sleep - 5;
+//
+//     this.play =  this.play - 5;
+//   }, 2000);
+// }
+
+// createItem(name) {
+//   const character = document.getElementById('character');
+//   const items = character.children;
+//   const mainItem = items[0];
+//   const itemCopy = mainItem.cloneNode(true);
+//   itemCopy.querySelector('span').innerHTML = `${name}`;
+//
+//   character.appendChild(itemCopy);
+// }
+
+// initCharacter () {
+//   const addName = document.getElementById('add-name');
+//   let inputName = addName.querySelector('input[type=text]');
+//   let buttonAdd = addName.querySelector('button');
+//
+//   buttonAdd.addEventListener('click', function () {
+//     let newName = inputName.value;
+//     if (newName !== '') {
+//       // this.createItem(newName);
+//
+//       inputName.value = '';
+//     }
+//   })
+// }
