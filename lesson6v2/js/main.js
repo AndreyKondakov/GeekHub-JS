53bbc61d705b99742fd1154bd5941d77c0757ff8
AndreyKondakov <primax7k@gmail.com>
2018-11-27 18:38:06 +0200

    lesson 6 v2: add item

--- lesson6v2/js/main.js
+++ lesson6v2/js/main.js
@@ -1,50 +1,81 @@
 class Character {
-  constructor(name) {
-    this.name = name;
-    this.walk = 99;
-    this.eat = 99;
-    this.sleep = 99;
-    this.play = 99;
+  constructor() {
+    // this.name = name;
+    this.walk = 80;
+    this.eat = 80;
+    this.sleep = 80;
+    this.play = 80;
 
-    this.initCharacter(name);
+    this.initCharacter();
     // this.createItem(name);
     // this.counter();
+
+    // делаем counter в конструкторе , что бы время было свое у каждого нового обьекта
   }
-  counter () {
-    setInterval(() => {
-      this.walk  = this.walk - 5;
-      this.eat =   this.eat - 5;
-      this.sleep = this.sleep - 5;
-      this.play =  this.play - 5;
-    }, 2000);
-  }
+  // counter () {
+  //   setInterval(() => {
+  //     this.walk  = this.walk - 5;
+  //     console.log(this.walk);
+  //     this.eat =   this.eat - 5;
+  //
+  //     this.sleep = this.sleep - 5;
+  //
+  //     this.play =  this.play - 5;
+  //   }, 2000);
+  // }
 
-  createItem(name) {
-    const character = document.getElementById('character');
-    const items = character.children;
-    const mainItem = items[0];
-    const itemCopy = mainItem.cloneNode(true);
-    itemCopy.querySelector('span').innerHTML = `${name}`;
-
-
-    character.appendChild(itemCopy);
-  }
-
+  // createItem(name) {
+  //   const character = document.getElementById('character');
+  //   const items = character.children;
+  //   const mainItem = items[0];
+  //   const itemCopy = mainItem.cloneNode(true);
+  //   itemCopy.querySelector('span').innerHTML = `${name}`;
+  //
+  //   character.appendChild(itemCopy);
+  // }
 
   initCharacter () {
     const addName = document.getElementById('add-name');
     let inputName = addName.querySelector('input[type=text]');
     let buttonAdd = addName.querySelector('button');
 
+    function createItem(name) {
+      const character = document.getElementById('character');
+      const items = character.children;
+      const mainItem = items[0];
+      const itemCopy = mainItem.cloneNode(true);
+      itemCopy.querySelector('span').innerHTML = `${name}`;
+
+      // this.counter();
+
+      character.appendChild(itemCopy);
+    }
+
     buttonAdd.addEventListener('click', function () {
       let newName = inputName.value;
       if (newName !== '') {
-        this.createItem(newName);
+        createItem(newName);
+        // console.log(this.newName, this.walk);
+
         inputName.value = '';
       }
     })
   }
 
+  // initCharacter () {
+  //   const addName = document.getElementById('add-name');
+  //   let inputName = addName.querySelector('input[type=text]');
+  //   let buttonAdd = addName.querySelector('button');
+  //
+  //   buttonAdd.addEventListener('click', function () {
+  //     let newName = inputName.value;
+  //     if (newName !== '') {
+  //       // this.createItem(newName);
+  //
+  //       inputName.value = '';
+  //     }
+  //   })
+  // }
 
 
   // walk() {
@@ -60,8 +91,10 @@ class Character {
   //
   // }
 }
+//
+// let ted = 'ted';
+//
+// var test = new Character(`${ted}`);
+// console.log(test.name);
 
-let ted = 'ted';
-
-var test = new Character(`${ted}`);
-console.log(test.name);
+let test = new Character();
