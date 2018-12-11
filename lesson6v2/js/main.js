5d6ebd1868f632f282f67133919da6a7f4a22c69
AndreyKondakov <primax7k@gmail.com>
2018-12-11 22:20:43 +0200

    lesson 6v2-x3: cleaning

--- lesson6v2/js/main.js
+++ lesson6v2/js/main.js
@@ -1,35 +1,25 @@
-// (function () {
 class Character {
   constructor(name) {
-    // this.createItem;
     this.name = name;
     this.walk = 100;
     this.eat = 100;
     this.sleep = 100;
     this.play = 100;
-
     const item = this.createItem();
 
-    // делаю counter в конструкторе , что бы время было свое у каждого нового обьекта
     this.counter = setInterval(() => {
-
-      this.walk  = this.walk - 5;
-      console.log('test walk 1', this.walk);
+      this.walk = this.walk - 5;
       let life = item.querySelector('.progress-life span');
       life.style.width = this.walk + '%';
-
-      this.eat =   this.eat - 6;
+      this.eat = this.eat - 6;
       let saturation = item.querySelector('.progress-saturation span');
       saturation.style.width = this.eat + '%';
-
       this.sleep = this.sleep - 7;
       let strength = item.querySelector('.progress-strength span');
       strength.style.width = this.sleep + '%';
-
-      this.play =  this.play - 10;
+      this.play = this.play - 10;
       let happiness = item.querySelector('.progress-happiness span');
       happiness.style.width = this.play + '%';
-
       // if ((this.walk && this.eat && this.sleep && this.play) <= 0 ) {   // if one of this value = 0
       if ((this.walk <= 0) && (this.eat <= 0) && (this.sleep <= 0) && (this.play <= 0)) {  // before all value = 0
         clearTimeout(this.counter);
@@ -39,7 +29,6 @@ class Character {
   }
 
   targetAction(item) {
-    // let character = document.getElementById('character');
     item.addEventListener('click', ({target}) => {
       if (target.tagName === 'BUTTON') {
         console.log(target.className);
@@ -53,8 +42,8 @@ class Character {
           this.sleep = this.sleep + 50;
           console.log('this is the sleep +50 = ', this.sleep)
         } else if (target.className === 'action-play') {
-          this.play = this.play + 5;
-          this.eat = this.eat - 10;
+          this.play = this.play + 15;
+          this.sleep = this.sleep - 10;
           console.log(this.walk, this.eat, this.sleep, this.play)
         }
       }
@@ -67,7 +56,6 @@ class Character {
     const mainItem = items[0];
     const itemCopy = mainItem.cloneNode(true);
     itemCopy.querySelector('span').innerHTML = `${this.name}`;
-    // this.counter();
     itemCopy.classList.remove('hidden');
     character.appendChild(itemCopy);
     this.targetAction(itemCopy);
@@ -75,82 +63,15 @@ class Character {
   };
 }
 
-
 const addName = document.getElementById('add-name');
 let inputName = addName.querySelector('input[type=text]');
 let buttonAdd = addName.querySelector('button');
 
-var countCharacter = 0;
-
 buttonAdd.addEventListener('click', function () {
   let newName = inputName.value;
   if (newName !== '') {
-    // eval('var z'+countCharacter+' = new Character(newName);');
     let person = new Character(newName);
-    // createItem(newName);
     console.log(this.name);
     inputName.value = '';
   }
-
-
-  // function createItem(name)  {
-  //   const character = document.getElementById('character');
-  //   const items = character.children;
-  //   const mainItem = items[0];
-  //   const itemCopy = mainItem.cloneNode(true);
-  //   itemCopy.querySelector('span').innerHTML = `${name}`;
-  //   // new Character(this.name);
-  //   // this.counter();
-  //   itemCopy.classList.remove('hidden');
-  //   character.appendChild(itemCopy);
-  //   targetAction();
-  // };
-  // setInterval(() => {
-  //
-  //   this.walk  = this.walk - 5;
-  //   console.log('test walk 1', this.walk);
-  //   let life = document.querySelector('.progress-life span');
-  //   life.style.width = this.walk + '%';
-  //
-  //   this.eat =   this.eat - 6;
-  //   let saturation = document.querySelector('.progress-saturation span');
-  //   saturation.style.width = this.eat + '%';
-  //
-  //   this.sleep = this.sleep - 7;
-  //   let strength = document.querySelector('.progress-strength span');
-  //   strength.style.width = this.sleep + '%';
-  //
-  //   this.play =  this.play - 10;
-  //   let happiness = document.querySelector('.progress-happiness span');
-  //   happiness.style.width = this.play + '%';
-  //
-  //   // if ((this.walk && this.eat && this.sleep && this.play) <= 0 ) {   // if one of this value = 0
-  //   if ((this.walk <= 0) && (this.eat <= 0) && (this.sleep <= 0) && (this.play <= 0)) {  // before all value = 0
-  //     clearTimeout(this.counter);
-  //   }
-  //   console.log(this.name);
-  // }, 2000);
-
-  // function targetAction() {
-  //   let character = document.getElementById('character');
-  //   character.addEventListener('click', ({target}) => {
-  //     if (target.tagName === 'BUTTON') {
-  //       console.log(target.className);
-  //       if (target.className === 'action-walk') {
-  //         this.walk = this.walk + 20;
-  //         console.log(target.className, '+20 = ', this.walk);
-  //       } else if (target.className === 'action-eat') {
-  //         this.eat = this.eat + 30;
-  //         console.log('eat +30 = ', this.eat)
-  //       } else if (target.className === 'action-sleep') {
-  //         this.sleep = this.sleep + 50;
-  //         console.log('this is the sleep +50 = ', this.sleep)
-  //       } else if (target.className === 'action-play') {
-  //         this.play = this.play + 5;
-  //         this.eat = this.eat - 10;
-  //         console.log(this.walk, this.eat, this.sleep, this.play)
-  //       }
-  //     }
-  //   })
-  // }
 });
