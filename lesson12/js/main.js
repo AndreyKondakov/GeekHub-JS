4fcd848154f5a4a38acc9c6ebf4f7e7c88b382a5
AndreyKondakov <primax7k@gmail.com>
2019-01-11 00:15:52 +0200

    lesson 12:  list all repositories

--- lesson12/js/main.js
+++ lesson12/js/main.js
@@ -1,35 +1,37 @@
-//
-// function getData(id) {
-//   fetch('https://api.github.com/users/AndreyKondakov/repos\n', {
-//     // method: 'get',
-//     // dataType: "json"
-//   }).then(dataWrappedByPromise => dataWrappedByPromise.json())
-//     .then(data => {
-//       // you can access your data here
-//       var getOneBranh = data[id];
-//       var getReposLastDate = getOneBranh['pushed_at'];
-//       var itemDate = document.getElementById('geekhubJs');
-//       var spanDate = itemDate.querySelector('.result-item');
-//       spanDate.innerHTML = getReposLastDate;
-//     }).catch(function(err) {
-//     // обработка ошибки
-//     console.log('Error!')
-//   });
-// }
-
 var geekhub = document.getElementById('btn-last-change');
 geekhub.addEventListener('click', function () {
   fetch('https://api.github.com/users/AndreyKondakov/repos\n', {
   }).then(dataWrappedByPromise => dataWrappedByPromise.json())
     .then(data => {
-      // you can access your data here
       var getOneBranh = data[13];
       var getReposLastDate = getOneBranh['pushed_at'];
       var itemDate = document.getElementById('geekhubJs');
       var spanDate = itemDate.querySelector('.result-item');
       spanDate.innerHTML = getReposLastDate;
     }).catch(function(err) {
-    // обработка ошибки
     console.log('Error!')
   });
 });
+
+var git = 0;
+var getLen = 0;
+
+function getData() {
+  fetch('https://api.github.com/users/AndreyKondakov/repos\n', {
+    // method: 'get',
+    // dataType: "json"
+  }).then(dataWrappedByPromise => dataWrappedByPromise.json())
+    .then(data => {
+      var list = document.getElementById('git-list');
+      for(let i=0; i<data.length; i++) {
+        let li = document.createElement('li');
+        li.innerHTML = data[i]['name'];
+        list.appendChild(li);
+      }
+    }).catch(function(err) {
+    console.log('Error!')
+  });
+};
+getData();
+
+
