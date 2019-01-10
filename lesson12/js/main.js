6f7132ebba59d03b447c30dd58d68b38d30cd605
AndreyKondakov <primax7k@gmail.com>
2019-01-10 19:05:59 +0200

    lesson 12: done (get data last changes)

--- lesson12/js/main.js
+++ lesson12/js/main.js
@@ -1,21 +1,35 @@
+//
+// function getData(id) {
+//   fetch('https://api.github.com/users/AndreyKondakov/repos\n', {
+//     // method: 'get',
+//     // dataType: "json"
+//   }).then(dataWrappedByPromise => dataWrappedByPromise.json())
+//     .then(data => {
+//       // you can access your data here
+//       var getOneBranh = data[id];
+//       var getReposLastDate = getOneBranh['pushed_at'];
+//       var itemDate = document.getElementById('geekhubJs');
+//       var spanDate = itemDate.querySelector('.result-item');
+//       spanDate.innerHTML = getReposLastDate;
+//     }).catch(function(err) {
+//     // обработка ошибки
+//     console.log('Error!')
+//   });
+// }
 
-var getReposLastDate;
-
-fetch('https://api.github.com/users/AndreyKondakov/repos\n', {
-  // method: 'get',
-  // dataType: "json"
-}).then(dataWrappedByPromise => dataWrappedByPromise.json())
-  .then(data => {
-    // you can access your data here
-    var getOneBranh = data[16];
-    getReposLastDate = getOneBranh['pushed_at'];
-    var itemDate = document.getElementById('geekhubJs');
-    var spanDate = itemDate.querySelector('.result-item');
-    spanDate.innerHTML = getReposLastDate;
-
-  }).catch(function(err) {
-  // обработка ошибки
-  console.log('Error!')
+var geekhub = document.getElementById('btn-last-change');
+geekhub.addEventListener('click', function () {
+  fetch('https://api.github.com/users/AndreyKondakov/repos\n', {
+  }).then(dataWrappedByPromise => dataWrappedByPromise.json())
+    .then(data => {
+      // you can access your data here
+      var getOneBranh = data[13];
+      var getReposLastDate = getOneBranh['pushed_at'];
+      var itemDate = document.getElementById('geekhubJs');
+      var spanDate = itemDate.querySelector('.result-item');
+      spanDate.innerHTML = getReposLastDate;
+    }).catch(function(err) {
+    // обработка ошибки
+    console.log('Error!')
+  });
 });
-
-
