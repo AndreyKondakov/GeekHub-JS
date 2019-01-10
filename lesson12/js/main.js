c33152ca25df870562888575a05fa307ad12b095
AndreyKondakov <primax7k@gmail.com>
2019-01-10 18:01:22 +0200

    lesson 12: get last update branch geekhub-JS

--- lesson12/js/main.js
+++ lesson12/js/main.js
@@ -1,18 +1,21 @@
 
+var getReposLastDate;
 
-
-var getReposResult = fetch('https://api.github.com/users/AndreyKondakov/repos\n', {
+fetch('https://api.github.com/users/AndreyKondakov/repos\n', {
   // method: 'get',
   // dataType: "json"
 }).then(dataWrappedByPromise => dataWrappedByPromise.json())
   .then(data => {
     // you can access your data here
     var getOneBranh = data[16];
-    console.log(getOneBranh)
+    getReposLastDate = getOneBranh['pushed_at'];
+    var itemDate = document.getElementById('geekhubJs');
+    var spanDate = itemDate.querySelector('.result-item');
+    spanDate.innerHTML = getReposLastDate;
+
   }).catch(function(err) {
   // обработка ошибки
   console.log('Error!')
 });
 
-// console.log(getReposResult);
 
