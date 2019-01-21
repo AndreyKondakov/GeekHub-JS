f6cf311ba4a764568992afde213a25614e8ce204
AndreyKondakov <primax7k@gmail.com>
2019-01-21 14:20:14 +0200

    lesson 14: first task (b) - for of (obj)

--- lesson14/main.js
+++ lesson14/main.js
@@ -1,4 +1,4 @@
-console.log('a) Написати нескінченний генератор випадкових чисел на основі ітераторів.')
+console.log('a) Написати нескінченний генератор випадкових чисел на основі ітераторів.');
 const gRandomNumbers = {
   [Symbol.iterator]() {
     let count = 0;
@@ -19,26 +19,66 @@ for (let random of gRandomNumbers) {
 }
 
 
+console.log('b) Написати функціонал для перебору через for .. of об\'єкта з декількома рівнями вкладеності.');
+
+Object.prototype[Symbol.iterator] = function() {
+  var keys = Object.keys(this)[Symbol.iterator]();
+  var obj = this;
+  var output;
+  return {next:function() {
+      if (!(output = keys.next()).done)
+        output.value = [output.value, obj[output.value]];
+      return output;
+    }};
+};
+
+let myObj = {
+  'первая вложеность1': '1',
+  test2: '2',
+  'первая вложеность3': '3',
+  'первая вложеность obj': {
+    'вторая вложеность4': '11',
+    test5: '22',
+    'вторая вложеность6': '33',
+  }
+};
+
+for (const [k, v] of myObj) {
+  console.log(`${k} : ${v}`);
+  if ((typeof  v) === 'object') {
+    for (const [n, t] of v) {
+      console.log(`${n} : ${t}`);
+    }
+  }
+  // else if ((typeof k) === 'object') {
+  //   for (const [n, t] of k) {
+  //     console.log(`${n} : ${t}`);
+  //   }
+  // }
+}
 
 
 
 
-// function perform() {
+
+
+// let myObj2 = {
+//   'первая вложеность1': '1',
+//   'первая вложеность2': '2',
+//   'первая вложеность3': '3',
+//   'первая вложеность obj': {
+//     'вторая вложеность4': '11',
+//     'вторая вложеность5': '22',
+//     'вторая вложеность6': '33',
+//   },
+//   [Symbol.iterator]: function* () {
+//     for (const i of Object.keys(this)) {
+//       yield [i, this[i]];
+//     }
+//   }
+// };
 //
-
+// for (const [k, v] of myObj2) {
+//   console.log(`${k} : ${v}`);
+//
 // }
-//
-// perform(null, function(value) { // value === null
-//   var param = 1;
-//   console.log(param); // 1
-//   return param;
-// })
-//   .then(function(param) { // param === 1
-//     console.log(++param); // 2
-//     return param;
-//   })
-//   .then(function(param) { // param === 2
-//     console.log(++param); // 3
-//     return param;
-// });
-
